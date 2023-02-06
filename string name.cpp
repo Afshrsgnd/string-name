bfbbd2261b64d23f4e31dbd741559d7089416a61
Afshrsgnd <124483567+Afshrsgnd@users.noreply.github.com>
2023-02-06 13:13:06 +0330

    Add files via upload

---
+++ string name.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	string name[5];
+	
+	cout<<"Enter 5 names : ";
+	
+	for (int i=0 ; i<5 ; i++)
+
+	cin>>name[i];
+	
+    for (int i=0 ; i<5 ; i++)
+
+	cout<<"Hi "<<name [i]<<" how are you?!"<<endl;
+}
+
